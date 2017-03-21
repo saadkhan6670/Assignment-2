fa0247a11252decb9b97166b0541f13a6158e00a
saadkhan6670 <170629minipoll>
2017-03-21 11:39:03 +0500

    Add existing file

--- User/controller.js
+++ User/controller.js
@@ -2,14 +2,12 @@
 
    exports.CreateUser = function (req, res) {
 
-        res.send( 'hooray! welcome to our rest video api!' );
 
-
-    };
-  /* exports.LoginUser = function (req, res ) {
+    }
+   exports.LoginUser = function (req, res ) {
 
 
     }
    exports.UserProfile = function (req, res) {
 
-    }*/
+    }
--- index.js
+++ index.js
@@ -3,8 +3,21 @@ var express = require('express');
 var router = require('./user');
 var app = express();
 
+var cache = require('memory-cache');
 
-app.use('/user', router);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/api/users', function(req, res) {
+    var username = req.body.username;
+    var email  = req.body.email ;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+
+    res.send(username + ' ' + email + ' ' + firstname+lastname);
+});
+
 
 
 app.listen(3000, function() {
