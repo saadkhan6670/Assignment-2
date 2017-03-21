9f7040cde8602b0018b688e2a049e1d47801fbcf
saadkhan6670 <170629minipoll>
2017-03-21 12:30:58 +0500

    new package.json file

--- index.js
+++ index.js
@@ -2,24 +2,11 @@
 var express = require('express');
 var router = require('./user');
 var app = express();
-
-var cache = require('memory-cache');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.post('/api/users', function(req, res) {
-    var username = req.body.username;
-    var email  = req.body.email ;
-    var FirstName = req.body.firstname;
-    var LastName = req.body.lastname;
-
-    cache.put('Username.memo', username);
-    console.log(cache.get('Username.memo'));
-
-    res.send(username + ' ' + email + ' ' + FirstName + ' ' + LastName);
-});
+app.use("/user",router);
 
 
 
