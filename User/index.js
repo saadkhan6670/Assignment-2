71b5c5bd59cdec309cdc2a9730a2ec1a38ee3153
saadkhan6670 <170629minipoll>
2017-03-22 15:00:45 +0500

    authentication added

--- User/index.js
+++ User/index.js
@@ -8,9 +8,14 @@ router.get("/",function(req,res){
     res.json("TO create account go to this link /user/create-user \n TO login the account go to this link /user/login-user  ");
 });
 
-router.post('/create-user',controller.CreateUser);
-router.post('/login-user', controller.LoginUser);
-router.get('/loggedin',controller.loggedin(req,res));
+router.get("/failed",function(req,res){
+    res.json("FAILED ");
+});
+router.post('/create-user',controller.CreateUser,controller.ShowEnteredUser);
+router.post('/login-user', controller.LoginUser,passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/failed'}));
+
+//router.get('/logged-in',controller.LoggedIn);
 router.get('/user-profile', controller.UserProfile);
 
 module.exports = router;
