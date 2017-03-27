8e392e4754b734f0f068b100ed54ec0e3e8f2994
saadkhan6670 <170629minipoll>
2017-03-27 11:43:37 +0500

    authentication added

--- User/controller.js
+++ User/controller.js
@@ -2,63 +2,41 @@
 var express = require('express');
 var app = express();
 var cache = require('memory-cache');
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-var cookieParser = require("cookie-parser");
-var session = require("express-session");
 
-var username ,password , email  ,FirstName,  LastName;
+var count=0 ;
+
 
    exports.CreateUser = function (req, res, next) {
-           username = req.body.username;
-       password = req.body.password;
-            email  = req.body.email ;
-            FirstName = req.body.firstname;
-            LastName = req.body.lastname;
 
-           cache.put('Username.memo', username);
-           console.log(cache.get('Username.memo'));
+       var User =[ {
+           firstName: '',
+           lastName: '',
+           email: '',
+           phone: '',
+           password:''
+       }];
 
-       cache.put('pass.memo', password);
-       console.log(cache.get('pass.memo'));
+       for (var user in User){
+           cache.put('User',req.body);
+         console.log( cache.get('User'));
 
-       cache.put('Email.memo', email);
-       console.log(cache.get('Email.memo'));
+           }
 
-       cache.put('FN.memo', FirstName);
-       console.log(cache.get('FN.memo'));
+       count++;
+       console.log(count);
 
-       cache.put('LN.memo', LastName);
-       console.log(cache.get('LN.memo'));
-       next();};
+
+       };
+
+
+       //res.send("First Name:\t"+ User.firstName + "\n Last Name:\t" + User.lastName +"\n Email: \t" + User.email + "\nPhone:\t"+User.phone + "\nPassword\t"+ User.password);
+
 
     exports.ShowEnteredUser = function (req,res) {
-        res.send("username:\t"+ username + "\n Password:\t" + password +"\n Email: \t" + email + "\nFirst Name:\t"+FirstName + "\nLast Name:\t"+ LastName);
+
 
     };
 
    exports.LoginUser = function (req, res ) {
 
-       app.use(session({secret: "some secret", resave: true,
-           saveUninitialized: true}));
-       app.use(cookieParser());
-       app.use(passport.initialize());
-       app.use(passport.session());
-
-       passport.use( new LocalStrategy( {
-               usernameField: 'username',
-               passwordField: 'password'
-           },
-           function(username, password, done)  {
-               exports.CreateUser.cache.put({ username:username }, function(err, user) {
-                   if (err) {throw err;}
-                   if (!user){
-                       return done(null, false, {message:'Unknown User'});}
-
-                   if (!user.validPassword(password)) {
-                       return done(null, false, { message: 'Incorrect password.' });
-                   }
-                   return done(null, user);
-               });
-           }));
-       };
+   };
--- User/index.js
+++ User/index.js
@@ -1,22 +1,17 @@
 'use strict';
 var router = require ('express').Router();
 var controller = require('./controller');
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
 
 router.get("/",function(req,res){
     res.json("TO create account go to this link /user/create-user \n TO login the account go to this link /user/login-user  ");
 });
 
-router.get("/failed",function(req,res){
-    res.json("FAILED ");
-});
-router.post('/create-user',controller.CreateUser,controller.ShowEnteredUser);
-router.post('/login-user', passport.authenticate('local', { successRedirect: '/',
-    failureRedirect: '/failed',
-    failureFlash: true }) , controller.LoginUser );
+router.post('/create-user',controller.CreateUser, function (req, res) {
+    console.log("User Created Success") });
+
+router.post('/login-user',controller.LoginUser );
 
 //router.get('/logged-in',controller.LoggedIn);
-router.get('/user-profile', controller.UserProfile);
+//router.get('/user-profile', controller.UserProfile);
 
 module.exports = router;
