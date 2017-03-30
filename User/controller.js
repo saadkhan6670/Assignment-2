e210ac0f0ffa390362546301bdd0677ca1eca8c6
saadkhan6670 <170629minipoll>
2017-03-30 12:36:28 +0500

    Create User and Login Added

--- User/controller.js
+++ User/controller.js
@@ -3,9 +3,8 @@ var express = require('express');
 var app = express();
 var cache = require('memory-cache');
 
-var count=0 ;
-
 cache.put('User',[]);
+var arr = cache.get('User');
 
    exports.CreateUser = function (req, res, next) {
 
@@ -17,43 +16,66 @@ cache.put('User',[]);
            password:''
        };
 
-       var arr = cache.get('User');
+       if (arr.length===0){
+           arr.push(req.body);
+           console.log("user " +arr.length+" created");
+       }
+
+       else{
+
        var NewEmail = req.body.email;
        var emailValidation;
-
-      // console.log("New User Email "+NewEmail);
+       var  Validity = false;
 
        arr.forEach( function (user)
        {
             emailValidation = user.email;
-          console.log(JSON.stringify(emailValidation));
 
+           if (NewEmail === emailValidation){
+               console.log({Error:"Cant use same email!"});
+               Validity =true;
+
+           }
        });
 
+       if (Validity===false) {
+           arr.push(req.body);
+           console.log("user " +arr.length+" created");
+       }}
 
-       if (NewEmail !== emailValidation){arr.push(req.body);}
-       else console.log({message:"Cant use same email!"});
-     if (arr.length===0){arr.push(req.body);}
-      //arr.push(req.body);
-
-
-//***********************************
-
-     //  http://procbits.com/2012/01/19/comparing-two-javascript-objects
-    //console.log( cache.get('User'));
-
-
-       res.send("user " +arr.length+" created");
+       res.send(cache.get('User'));
 
    };
 
-
-    exports.ShowEnteredUser = function (req,res) {
-
-        res.send("First Name:\t"+ User.firstName + "\n Last Name:\t" + User.lastName +"\n Email: \t" + User.email + "\nPhone:\t"+User.phone + "\nPassword\t"+ User.password);
-
-    };
 
    exports.LoginUser = function (req, res ) {
 
+       var loginUser = {
+           LogEmail:'',
+           LogPassword:''
+       };
+
+       var Credentials = req.body;
+       var emailValidation,passValidation;
+       var  Validity = false;
+
+       arr.forEach( function (login)
+       {
+           emailValidation =  login.email;
+           passValidation = login.password;
+
+           if ( Credentials.LogEmail === emailValidation){
+               if(Credentials.LogPassword===passValidation)
+               console.log("User Login Succes");
+               Validity =true;
+               res.end("Welcome USER");
+           }
+       });
+
+       if (Validity===false) {
+           console.log("Invalid credentials");
+           res.end();
+ }
    };
+
+
